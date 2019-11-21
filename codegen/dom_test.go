6b369588c199cc0bd79242538f7c255255655e59
Svetlin Ralchev <svet@ralch.com>
2019-11-21 12:52:17 +0000

    WIP

---
+++ codegen/dom_test.go
@@ -0,0 +1,69 @@
+package codegen_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("File", func() {
+	Describe("Literal", func() {
+		Describe("Commentf", func() {
+		})
+
+		Describe("Name", func() {
+		})
+
+		Describe("Element", func() {
+		})
+	})
+
+	Describe("Array", func() {
+		Describe("Commentf", func() {
+		})
+
+		Describe("Name", func() {
+		})
+
+		Describe("Element", func() {
+		})
+	})
+
+	Describe("Struct", func() {
+		Describe("Commentf", func() {
+		})
+
+		Describe("Name", func() {
+		})
+
+		Describe("AddField", func() {
+		})
+
+		Describe("Function", func() {
+			Describe("Commentf", func() {
+			})
+
+			Describe("Name", func() {
+			})
+
+			Describe("AddReceiver", func() {
+			})
+
+			Describe("AddParam", func() {
+			})
+
+			Describe("AddReturn", func() {
+			})
+
+			Describe("Body", func() {
+				Describe("Write", func() {
+				})
+
+				Describe("WriteComment", func() {
+				})
+
+				Describe("Build", func() {
+				})
+			})
+		})
+	})
+})
