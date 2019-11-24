0a9f7a20ac103a5e592113b54290a5127e7071d1
Svetlin Ralchev <svet@ralch.com>
2019-11-24 14:31:24 +0000

    WIP

--- plugin/golang/dom_merge_test.go
+++ plugin/golang/dom_merge_test.go
@@ -1,6 +1,8 @@
 package golang_test
 
 import (
+	"bytes"
+	"io/ioutil"
 	"os"
 
 	. "github.com/onsi/ginkgo"
@@ -9,11 +11,74 @@ import (
 	"github.com/phogolabs/stride/plugin/golang"
 )
 
-var _ = PDescribe("Merge", func() {
+var _ = Describe("Merge", func() {
 	var merger *golang.Merger
 
 	BeforeEach(func() {
 		merger = &golang.Merger{}
+	})
+
+	FDescribe("Struct", func() {
+		Context("when the struct is auto-generated", func() {
+			BeforeEach(func() {
+				target, err := golang.OpenFile("../../fixture/code/struct_generated_target.go.fixture")
+				Expect(err).To(BeNil())
+
+				source, err := golang.OpenFile("../../fixture/code/struct_generated_source.go.fixture")
+				Expect(err).To(BeNil())
+
+				merger.Target = target
+				merger.Source = source
+			})
+
+			It("appends the user-defined fields", func() {
+				Expect(merger.Merge()).To(Succeed())
+
+				target := &bytes.Buffer{}
+				merger.Target.WriteTo(target)
+
+				merged, err := ioutil.ReadFile("../../fixture/code/struct_generated_merged.go.fixture")
+				Expect(err).To(BeNil())
+
+				Expect(target.String()).To(Equal(string(merged)))
+			})
+		})
+
+		Context("when the struct is user-defined", func() {
+			BeforeEach(func() {
+				target, err := golang.OpenFile("../../fixture/code/struct_defined_target.go.fixture")
+				Expect(err).To(BeNil())
+
+				source, err := golang.OpenFile("../../fixture/code/struct_defined_source.go.fixture")
+				Expect(err).To(BeNil())
+
+				merger.Target = target
+				merger.Source = source
+			})
+
+			It("appends the struct to the end of the file", func() {
+				Expect(merger.Merge()).To(Succeed())
+
+				target := &bytes.Buffer{}
+				merger.Target.WriteTo(target)
+
+				merged, err := ioutil.ReadFile("../../fixture/code/struct_defined_merged.go.fixture")
+				Expect(err).To(BeNil())
+
+				// fmt.Println(target.String())
+				// fmt.Println("----------")
+				// fmt.Println(string(merged))
+
+				Expect(target.String()).To(Equal(string(merged)))
+			})
+		})
+	})
+
+	Describe("Function", func() {
+		Context("when the struct is initialized by the user", func() {
+			It("preservs the initialization", func() {
+			})
+		})
 	})
 
 	Context("when the range is in the beginning", func() {
@@ -52,7 +117,7 @@ var _ = PDescribe("Merge", func() {
 		})
 	})
 
-	FContext("when the range is in the middle", func() {
+	Context("when the range is in the middle", func() {
 		BeforeEach(func() {
 			target, err := golang.OpenFile("../fixture/merge_middle_target.go.fixture")
 			Expect(err).To(BeNil())
